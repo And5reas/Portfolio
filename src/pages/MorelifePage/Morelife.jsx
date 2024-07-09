47e1983ff85f9f934482238b1958b5fb0e9eea39
And5reas <And5reas.porcel@hotmail.com>
2024-07-08 22:10:27 -0300

    Adjusting name items

--- src/pages/MorelifePage/Morelife.jsx
+++ src/pages/MorelifePage/Morelife.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-import style from "./MorelifePage.module.css";
+import style from "./Morelife.module.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
